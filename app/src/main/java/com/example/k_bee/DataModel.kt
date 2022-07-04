e684a2d3f15a0207c406712a078f92831104cbb2
nakyung128 <lnk12888@naver.com>
2022-07-04 18:11:25 +0900

    실시간 DB uid로 구분

---
+++ app/src/main/java/com/example/k_bee/DataModel.kt
@@ -0,0 +1,17 @@
+package com.example.k_bee
+
+data class DataModel (
+    var AllCheck : Boolean = false,
+    var IsCheck1 : Boolean = false,
+    var IsCheck2 : Boolean = false,
+    var IsCheck3 : Boolean = false,
+    var IsCheck4 : Boolean = false,
+    var IsCheck5 : Boolean = false,
+    var IsCheck6 : Boolean = false,
+    var todo1 : String = "",
+    var todo2 : String = "",
+    var todo3 : String = "",
+    var todo4 : String = "",
+    var todo5 : String = "",
+    var todo6 : String = ""
+    )

eb491d8e1a91c843c7cc43c8b52b7696d76d93e4
Dahye Jeong <0dahye0>
2022-07-04 22:50:13 +0900

    하단 메뉴 네비게이션 아이콘 이미지 추가

--- app/src/main/java/com/example/k_bee/BadgeActivity.kt
+++ app/src/main/java/com/example/k_bee/BadgeActivity.kt
@@ -14,11 +14,10 @@ class BadgeActivity : AppCompatActivity() {
 
         lateinit var badgeList : Array<ImageView>
 
-
         val length = badgeList.size
 
-        for (i in 0 until length)
-        {
+
+        for (i in 0 until length) {
             // 배지 이미지 아이디 식별 (1-30)
             val badgeID = "badge" + (i + 1)
             val resID = resources.getIdentifier(badgeID, "id", packageName)
@@ -28,7 +27,19 @@ class BadgeActivity : AppCompatActivity() {
             val byteArray = intent.getByteArrayExtra("badge")
             val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
             badgeList[i].setImageBitmap(bitmap)
+
         }
+
+/*
+        val badge = findViewById<View>(R.id.badge1) as ImageView
+
+
+        val byteArray = intent.getByteArrayExtra("badge")
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+
+        badge.setImageBitmap(bitmap)*/
+
+
 
     }
 

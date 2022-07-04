67d9b9c53931ee71ae43145646b0cb79a4909efd
Dahye Jeong <0dahye0>
2022-07-04 21:10:37 +0900

    배지도감 배지 이미지 순차적으로 불러오기

--- app/src/main/java/com/example/k_bee/BadgeActivity.kt
+++ app/src/main/java/com/example/k_bee/BadgeActivity.kt
@@ -1,29 +1,35 @@
 package com.example.k_bee
 
 import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class BadgeActivity : AppCompatActivity() {
-
-    val badgeList = ArrayList<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_badge)
 
-        // 배지 이미지 아이디
-        // val badge = resources.getIdentifier("badge", "int", "k_bee")
+        lateinit var badgeList : Array<ImageView>
 
-        val badge = findViewById<View>(R.id.badge1) as ImageView
 
-        val byteArray = intent.getByteArrayExtra("badge")
-        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+        val length = badgeList.size
 
-        badge.setImageBitmap(bitmap)
+        for (i in 0 until length)
+        {
+            // 배지 이미지 아이디 식별 (1-30)
+            val badgeID = "badge" + (i + 1)
+            val resID = resources.getIdentifier(badgeID, "id", packageName)
+            badgeList[i] = findViewById<View>(resID) as ImageView
 
-}
+            // ShareActivity 에서 달성 배지 받아오기
+            val byteArray = intent.getByteArrayExtra("badge")
+            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
+            badgeList[i].setImageBitmap(bitmap)
+        }
+
+    }
 
 }

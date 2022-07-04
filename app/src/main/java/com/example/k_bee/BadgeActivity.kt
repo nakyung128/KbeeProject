e9a6cb3a543a36de57d3a287d4541c7a61edbaf9
Dahye Jeong <0dahye0>
2022-07-04 19:37:22 +0900

    splash screen 일단은 앱 로고만 (배경, 움짤 아직 추가 안함) 다시 푸쉬

--- app/src/main/java/com/example/k_bee/BadgeActivity.kt
+++ app/src/main/java/com/example/k_bee/BadgeActivity.kt
@@ -8,16 +8,16 @@ import android.widget.ImageView
 
 class BadgeActivity : AppCompatActivity() {
 
+    val badgeList = ArrayList<ImageView>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_badge)
-
 
         // 배지 이미지 아이디
         // val badge = resources.getIdentifier("badge", "int", "k_bee")
 
         val badge = findViewById<View>(R.id.badge1) as ImageView
-
 
         val byteArray = intent.getByteArrayExtra("badge")
         val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray!!.size)
